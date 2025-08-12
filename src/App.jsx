0feb041481382849082473ce522f079d75668abe
Harson <hassandaf07@gmail.com>
2025-08-12 12:47:42 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-900">
+      <h1 className="text-4xl font-bold text-white">Hello JobTrackr 🚀</h1>
+    </div>
+  )
+}
+
+export default App
+  
