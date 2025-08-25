93bc64d1ade9c4d29814f63280bc0915e312f82c
Harson <hassandaf07@gmail.com>
2025-08-25 17:11:15 +0100

    implement Login and Register components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Login from "./components/Login/Login"
+
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-900">
-      <h1 className="text-4xl font-bold text-white">Hello JobTrackr 🚀</h1>
-    </div>
+    <>
+      <Login />
+    </>
   )
 }
 
