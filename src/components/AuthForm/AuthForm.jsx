aa1522ec6664a6d9913c84e5e1c3ea8eb410b788
Harson <hassandaf07@gmail.com>
2025-08-28 13:13:42 +0100

    implement logout button to call laravel endpoint and end user session

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,30 +1,51 @@
-import LoginForm from "../Login/LoginForm"
 import { useState } from "react";
+import LoginForm from "../Login/LoginForm";
 
+const API_BASE = import.meta.env.VITE_API_BASE_URL;
 
-function AuthForm () {
+function AuthForm() {
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState(() => localStorage.getItem("token"));
 
-    function handleLoginSuccess(data) {
-        setUser(data.user);
-        setToken(data.token);
+  function handleLoginSuccess(data) {
+    setUser(data.user);
+    setToken(data.token);
+    localStorage.setItem("token", data.token);
+  }
 
-        localStorage.setItem('token', data.token);
+  async function handleLogout() {
+    try {
+        const res = fetch(`${API_BASE}/logout`, {
+            method: 'POST',
+            headers: {
+                Authorization: `Bearer ${token}`,
+                Accept: 'application/json'
+            },
+        });
+
+        if (!res.ok) {
+            console.error('Logout failed:'. res.status);
+        }
+    } catch (err) {
+        console.error('Logout error:', err); 
+    } finally {
+        setUser(null);
+        setToken(null);
+
+        localStorage.removeItem('token');
     }
+  }
 
-
-    const [user, setUser] = useState(null);
-    const [token, setToken] = useState(null);
-    const [jobs, setJobs] = useState([]);
-
-    return token ? (
-        <div>
-            <h2>Welcome, {user?.name}!</h2>
-            <p>You are logged in.</p>
-            <button>Logout</button>
-        </div>
-    ) : (
-        <LoginForm onSuccess={handleLoginSuccess} />
-    );
+  return token ? (
+    <div>
+      <h2>Welcome, {user?.name}!</h2>
+      <p>You are logged in.</p>
+      <button onClick={handleLogout}>Logout</button>
+    </div>
+  ) : (
+    <LoginForm onSuccess={handleLoginSuccess} />
+  );
 }
+
 
 export default AuthForm;
