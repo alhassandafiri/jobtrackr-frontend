d3bafa2b56ffcb11a0d51112199c126adc4cd80a
Harson <hassandaf07@gmail.com>
2025-08-28 12:32:53 +0100

    implement login success handling and update AuthForm rendering logic

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -3,15 +3,28 @@ import { useState } from "react";
 
 
 function AuthForm () {
+
+    function handleLoginSuccess(data) {
+        setUser(data.user);
+        setToken(data.token);
+
+        localStorage.setItem('token', data.token);
+    }
+
+
     const [user, setUser] = useState(null);
     const [token, setToken] = useState(null);
     const [jobs, setJobs] = useState([]);
 
-    return(
-        <>
-          <LoginForm />
-        </>
-    )
+    return token ? (
+        <div>
+            <h2>Welcome, {user?.name}!</h2>
+            <p>You are logged in.</p>
+            <button>Logout</button>
+        </div>
+    ) : (
+        <LoginForm onSuccess={handleLoginSuccess} />
+    );
 }
 
 export default AuthForm;
