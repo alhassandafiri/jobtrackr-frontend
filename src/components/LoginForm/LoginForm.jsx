22e9df320e8a6a835054d0a175ed1369d955c8cc
Harson <hassandaf07@gmail.com>
2025-08-31 12:42:26 +0100

    remove shadows from login and register tabs

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -62,7 +62,7 @@ function LoginForm ({onSuccess}) {
 
 
     return(
-        <div className="max-w-md w-full bg-white shadow-xl rounded-2xl p-6">
+        <div className="max-w-md w-full bg-white rounded-2xl p-6">
             <h1 className="text-2xl font-semibold mb-6">
                 Welcome Back
             </h1>
--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -12,7 +12,7 @@ function RegisterForm () {
     }
 
     return(
-        <div className="max-w-md w-full bg-white shadow-xl rounded-2xl p-6">
+        <div className="max-w-md w-full bg-white rounded-2xl p-6">
             <h1 className="text-2xl font-semibold mb-6">
                 Create an account
             </h1>
