4a2dfea8a84ea4420d243bef15b3fa2effc8a0a0
Harson <hassandaf07@gmail.com>
2025-09-01 13:04:27 +0100

    add routing and auth context imports in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
+import { AuthProvider } from "./components/AuthContext/AuthContext"
+import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute"
 import AuthForm from "./components/AuthForm/AuthForm"
 import Logo from "./components/Logo/Logo"
 
