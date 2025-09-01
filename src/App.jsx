59e86beb6dd703f773da38e5d41f8b23847f31cc
Harson <hassandaf07@gmail.com>
2025-09-01 13:47:18 +0100

    refactor App component to integrate Kanban and streamline routing structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-
 import { AuthProvider } from "./components/AuthContext/AuthContext"
 import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute"
 import AuthForm from "./components/AuthForm/AuthForm"
 import Logo from "./components/Logo/Logo"
+import Kanban from "./components/Kanban/Kanban"
 
 function App() {
   return (
-    <>
-      <div className="absolute top-6 left-6">
-        <Logo />
-      </div>
-      <div className="pt-32">
-        <AuthForm />
-      </div>
-    </>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/auth" element={<AuthForm />} />
+          <Route path="/" element={ <ProtectedRoute> <Kanban /> </ProtectedRoute> } />
+
+        </Routes>
+      </Router>
+    </AuthProvider>
   )
 }
 
 export default App
-  
+
